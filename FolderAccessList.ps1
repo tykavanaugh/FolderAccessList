550c1d30784397285c1cf11cb34f2e4df71c73f3
Ty K <tykavanaugh@gmail.com>
2021-05-05 13:43:16 -0400

    Add files via upload

---
+++ FolderAccessList.ps1
@@ -0,0 +1,22 @@
+﻿$folderName = Read-Host -Prompt 'Input folder name'
+$output = "Groups with access:`n"
+net use S:
+Set-Location \\hgfs2\Shared\
+
+$accessGroupsRaw = (Get-Acl "\\hgfs2\Shared\$foldername").Access
+
+$i = 0
+for (;$i -le ($accessGroupsRaw.Count - 1);$i++){
+    $nameRaw = $accessGroupsRaw.IdentityReference[$i]
+    $name = $nameRaw.Value.Split('\')[1]
+    if (($name -ne "Administrators") -and ($name -ne "Domain Admins")){
+        $output = $output + $name + "`n---------`n"
+        $members = Get-ADGroupMember -Identity $name | select Name
+        foreach($member in $members){
+            $output = $output + $member.Name + "`n"
+            }
+        $output = $output + $members + "`n"
+        }
+    }
+
+
